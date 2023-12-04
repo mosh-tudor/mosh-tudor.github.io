a4800ba36ad6c380a2b69000fe01a94ecf34ca1e
Тudоr <668451+mosh-tudor@users.noreply.github.com>
2023-12-04 14:57:47 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,8 @@
-alert('test');
+const params = new URLSearchParams(window.location.search);
+
+if (params.has('invite')) {
+  const inviteCode = params.get('invite');
+  const recirectUrl = `https://republic.com/note/giveaway?invite=${inviteCode}`;
+
+  window.location.href = `${window.location.pathname}?after_subscribe_redirect=${recirectUrl}`;
+}
