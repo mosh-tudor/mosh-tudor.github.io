292fcb0987159576efe90e4c6c1fe908e61bb4b6
Тudоr <668451+mosh-tudor@users.noreply.github.com>
2023-12-04 19:47:17 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -3,6 +3,13 @@ const params = new URLSearchParams(window.location.search);
 if (params.has('invite')) {
   const inviteCode = params.get('invite');
   const recirectUrl = `https://republic.com/note/giveaway?invite=${inviteCode}`;
+  const lpbBody = document.querySelector(".landing_pages-show");
+  const subscriptions = lpbBody.dataset.subscriptions?.split(',') || []
+  const noteCategory = "note_2023";
 
-  window.location.href = `${window.location.pathname}?after_subscribe_redirect=${recirectUrl}`;
+  if (subscriptions.includes(noteCategory)) {
+    window.location.href = recirectUrl;
+  } else {
+    window.location.href = `${window.location.pathname}?after_subscribe_redirect=${recirectUrl}`;
+  }
 }
